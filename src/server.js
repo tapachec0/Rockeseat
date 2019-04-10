ab460dc6470f614af274c8a491c72a78cf4fa6cb
tapachec0 <talytapacheco.1c2015@gmail.com>
2019-04-10 14:57:05 -0300

    Consertado o import do cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 /*store all aplication info*/
 const app = express();
 
-app.use(cors);
+app.use(cors());
 
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
