10d63ecfb85839a2c801763cf1891ba95e21e97f
Talyta Pacheco <31139458+tapachec0@users.noreply.github.com>
2019-04-13 02:05:32 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -36,4 +36,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/files", express.static(path.resolve(__dirname, '..', 'tmp')));
 app.use(require('./routes'));
-server.listen(process.env.URL || 3333);
+server.listen(process.env.PORT || 3333);
